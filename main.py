656064a2054952471ac95daf627af5a6e8b5cf4c
Josh <josh.hackney13@gmail.com>
2020-12-17 17:32:39 -0500

    updated td module to check market status, backing code is mostly done so now can go about getting the stock data

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pytz
 import configparser
 import eodata
+import td
 import os.path
 from os import path
 import json
@@ -8,7 +9,7 @@ from datetime import datetime, timedelta
 
 # Get configuration
 conf = configparser.ConfigParser()
-conf.read('data/conf.ini')
+conf.read('data/conf.ini')          # unfortunately don't see a way around hardcoding this value
 
 # Set constants from config
 timezone        = conf['GLOBAL']['timezone']
@@ -20,34 +21,24 @@ tickFile        = dataDir + conf['GLOBAL']['tickFile']
 # Pass True to create file data/tickers.json with a list of tickers and a dict containing "tickers":"company name"
 # Pass False to only return a list of tickers
 
-if path.exists(tickFile):
-    print("found tickers.json, loading file")
-    with open(tickFile) as f:
-      tickers = json.load(f)
-      tickers = tickers['tickers']
-else:
-    if createTickFile:
-      print("tickers.json not found, populating")
-      tickers = eodata.populateTickers(dataDir, True)
+def loadTickers():
+    if path.exists(tickFile):
+        print("found tickers.json, loading file")
+        with open(tickFile) as f:
+          tickers = json.load(f)
+          tickers = tickers['tickers']
     else:
-      print("createTickFile false, only returning tickers")
-      tickers = eodata.populateTickers(dataDir, False)
+        if createTickFile:
+          print("tickers.json not found, populating")
+          tickers = eodata.populateTickers(dataDir, True)
+        else:
+          print("createTickFile false, only returning tickers")
+          tickers = eodata.populateTickers(dataDir, False)
+    return(tickers)
 
-
-
-
-today = datetime.today().astimezone(pytz.timezone(timezone))
-today_fmt = today.strftime('%Y-%m-%d')
-
-# Call the td ameritrade hours endpoint for equities to see if it is open
-market_url = 'https://api.tdameritrade.com/v1/marketdata/EQUITY/hours'
-
-params = {
-    'apikey': api_key,
-    'date': today_fmt
-    }
-
-request = requests.get(
-    url=market_url,
-    params=params
-    ).json()
+if __name__ == "__main__":
+    tickers = loadTickers()
+    if td.marketOpen():
+      print("Market Open!")
+    else:
+      print("Market Closed...")
--- td/td.py
+++ td/td.py
@@ -1,39 +1,71 @@
 import time
+import pytz
+import configparser
 from datetime import datetime, timedelta
-
 import requests
+
+conf = configparser.ConfigParser()
+conf.read("data/conf.ini")
+
+
+# Call the td ameritrade hours endpoint for equities to see if it is open
+def marketOpen():
+  """
+  Return True or False if stock market was open today
+  """
+  tz          = conf['GLOBAL']['timezone']
+  today       = datetime.today().astimezone(pytz.timezone(tz))
+  today_fmt   = today.strftime('%Y-%m-%d')
+  tdHoursURL  = conf['TD']['hoursURL']
+  key         = conf['TD']['key']
+  params = {
+    'apikey': key,
+    'date': today_fmt
+    }
+
+  request = requests.get(
+    url=tdHoursURL,
+    params=params
+    ).json()
+  
+  
+  if request['equity']['EQ']['isOpen'] is True:
+    return(True)
+  else:
+    return(False)
 
 
 # The TD Ameritrade api has a limit to the number of symbols you can get data for
 # in a single call so this function breaks the tlist (ticker list) up into 'n' length chunks 
 def chunks(tlist, n):
-    """
-    Takes in a list and how long you want
-    each chunk to be
-    """
-    n = max(1, n)
-    return (tlist[i:i+n] for i in range(0, len(tlist), n))
+  """
+  Takes in a list and how long you want
+  each chunk to be
+  """
+  n = max(1, n)
+  return (tlist[i:i+n] for i in range(0, len(tlist), n))
+
 
 def quotes_request(ticker, api_key, params):
-    """
-    Makes an api call for a list of stock symbols
-    and returns a dataframe
-    """
-    url = r"https://api.tdameritrade.com/v1/marketdata/quotes"
+  """
+  Makes an api call for a list of stock symbols
+  and returns a dataframe
+  """
+  url = r"https://api.tdameritrade.com/v1/marketdata/quotes"
 
-    params = {
-    'apikey': api_key,
-    'symbol': stocks
-    }
+  params = {
+  'apikey': api_key,
+  'symbol': stocks
+  }
 
-    request = requests.get(
-        url=url,
-        params=params
-        ).json()
+  request = requests.get(
+    url=url,
+    params=params
+    ).json()
 
-    time.sleep(1)
+  time.sleep(1)
 
-    return pd.DataFrame.from_dict(
-        request,
-        orient='index'
-        ).reset_index(drop=True)
+  return pd.DataFrame.from_dict(
+    request,
+    orient='index'
+    ).reset_index(drop=True)
